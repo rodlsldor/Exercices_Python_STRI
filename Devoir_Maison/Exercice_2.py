321ad53b547d175ac89f2996083a6ca5f70fe70f
rodlsldor <rayan.dakhel@yahoo.com>
2022-10-03 14:49:40 +0200

    Fin Exercice 2

---
+++ Devoir_Maison/Exercice_2.py
@@ -0,0 +1,20 @@
+from itertools import count
+
+
+def adnValide():
+    chaine = input()
+    for i in range(len(chaine)-1):
+        if (chaine[i] not in ('a', 'c', 't', 'g')):
+            adnValide()
+    return chaine
+
+
+def occAdn():
+    print("Entrez une chaine ADN:\n")
+    chaine = str(adnValide())
+    print("Entrez la sous chaine ADN:\n")
+    sschaine = str(adnValide())
+    return chaine.count(sschaine)
+
+
+print(occAdn())
