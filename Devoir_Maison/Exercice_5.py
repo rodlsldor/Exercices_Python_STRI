9caef902fbb5f25ca6e84bfbee4f23aa70b1ea81
rodlsldor <rodls.dakhel@gmail.com>
2022-10-23 11:33:16 +0200

    fin exercice 5

--- Devoir_Maison/Exercice_5.py
+++ Devoir_Maison/Exercice_5.py
@@ -1,20 +1,56 @@
-def create_pile(liste):
-    return liste
+from msilib import CreateRecord
 
 
-def empiler(liste, num):
-    return liste.append(num)
+def isempty(l):
+    if (len(l) == 0):
+        print("La pile/file est vide")
+        return True
+    return False
 
 
-def depiler(liste):  # enlève un élement de la pile
-    if (liste.length == 0):
-        return -1
-    else:
-        return liste.pop()
+def create_pile(l):
+    pile = []
+    for i in range(len(l)):
+        pile.append(l[i])
+    return pile
 
 
-def defiler(liste):  # enlève un élement de la file
-    if (liste.length == 0):
-        return -1
-    else:
-        return liste.pop(0)
+def empiler(p, x):
+    p.append(x)
+
+
+def depiler(p):  # enlève un élement de la pile
+    if (not isempty(p)):
+        p.pop()
+
+
+def creer_file(l):
+    file = []
+    for i in range(len(l)):
+        file.append(l[i])
+    return file
+
+
+def enfiler(f, x):
+    f.append(x)
+
+
+def defiler(f):  # enlève un élement de la file
+    if (not isempty(f)):
+        f.pop(0)
+
+
+pile = create_pile([1, 2, 3, 4, 5])
+print(pile)
+empiler(pile, 7)
+print(pile)
+depiler(pile)
+print(pile)
+
+file = creer_file([])
+print(file)
+enfiler(file, 8)
+print(file)
+defiler(file)
+print(file)
+defiler(file)
